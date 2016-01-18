b0f0d715ad95058d2b9c877ccf42a573cf68b6e0
niek <niek_1993@hotmail.com>
2016-01-18 23:55:37 +0100

    Webservice helemaal gefixed nu

--- SE42 auction/CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
+++ SE42 auction/CalculatorWS_Client_Application/src/calculatorws_client_application/CalculatorWS_Client_Application.java
@@ -20,13 +20,29 @@ public class CalculatorWS_Client_Application {
         int i = 3;
         int j = 4;
         int result = add(i, j);
-        System.out.println("Result = " + result);
+        System.out.println("Add = " + result);
+        result = minus(j, i);
+        System.out.println("Minus = " + result);
+        result = times(i, j);
+        System.out.println("Times = " + result);
     }
 
     private static int add(int arg0, int arg1) throws NegativeNumberException_Exception {
         webservice.WebCalculatorService service = new webservice.WebCalculatorService();
         webservice.WebCalculator port = service.getWebCalculatorPort();
         return port.add(arg0, arg1);
+    }
+
+    private static int minus(int arg0, int arg1) throws NegativeNumberException_Exception {
+        webservice.WebCalculatorService service = new webservice.WebCalculatorService();
+        webservice.WebCalculator port = service.getWebCalculatorPort();
+        return port.minus(arg0, arg1);
+    }
+
+    private static int times(int arg0, int arg1) throws NegativeNumberException_Exception {
+        webservice.WebCalculatorService service = new webservice.WebCalculatorService();
+        webservice.WebCalculator port = service.getWebCalculatorPort();
+        return port.times(arg0, arg1);
     }
 
 }
